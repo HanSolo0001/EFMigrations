86d02838999aead5ae0901cb449e66489a44f26b
HanSolo0001 <pillarite@gmail.com>
2017-03-21 01:43:02 -0400

    Added database initializer for shared environments using MigrateDatabaseToLatestVersion, added ComicBookAverageRating class and created relationship between ComicBook class and ComicBookAverageRating class, and performed update-database commands to generate SQL script files

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBookAverageRating.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBookAverageRating.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
-    class ComicBookAverageRating
+    public class ComicBookAverageRating
     {
+        public int Id { get; set; }
+        public int ComicBookId { get; set; }
+        public decimal AverageRating { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// comic book navigation property associating the one-to-many relationship between comic books and their average ratings. Comic books are the one; one comic book can have multiple ratings, which is why the AverageRating class is the many part of the relationship.
+        /// </summary>
+        public ComicBook ComicBook { get; set; }
     }
 }
